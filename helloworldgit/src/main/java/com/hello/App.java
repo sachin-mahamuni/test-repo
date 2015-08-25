62eb2bbe35cecedec94b82aa166886528592675f
sachin-mahamuni <sachinmahamuni99@gmail.com>
2015-08-25 17:43:39 +0530

    Git message

--- helloworldgit/src/main/java/com/hello/App.java
+++ helloworldgit/src/main/java/com/hello/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World4!" );
+        System.out.println( "GitHub Jenkins job creation!" );
         
     }
 }
